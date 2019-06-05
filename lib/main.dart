31ec2156b87595719f110d773b70556cc2b3a135
Mahima MS <mahima@stasislabs.com>
2019-06-05 09:07:35 +0530

    add action list items to nav drawer

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,23 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  var titleBarContent = "Primary";
+
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       drawer: getMailDrawer(),
       appBar: AppBar(
         title: Text(
-          "Primary",
+          titleBarContent,
           style: TextStyle(
               fontWeight: FontWeight.w400, color: Colors.white, fontSize: 20.0),
         ),
@@ -126,7 +135,6 @@ class MyHomePage extends StatelessWidget {
   }
 
   Drawer getMailDrawer() {
-    
     Text name = new Text(
       "Harry",
       style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15.0),
@@ -136,19 +144,79 @@ class MyHomePage extends StatelessWidget {
       "harry@gmail.com",
       style: TextStyle(fontWeight: FontWeight.w500, fontSize: 15.0),
     );
-    
+
+    var drawerIcons = [
+      Icon(Icons.move_to_inbox),
+      Icon(Icons.inbox),
+      Icon(Icons.people),
+      Icon(Icons.local_offer),
+      Icon(Icons.star),
+      Icon(Icons.access_time),
+      Icon(Icons.label),
+      Icon(Icons.send),
+      Icon(Icons.send),
+      Icon(Icons.note),
+      Icon(Icons.mail),
+      Icon(Icons.error),
+      Icon(Icons.delete),
+      Icon(Icons.label),
+      Icon(Icons.label),
+      Icon(Icons.settings),
+      Icon(Icons.help),
+    ];
+
+    var drawerText = [
+      "All inboxes",
+      "Primary",
+      "Social",
+      "Promotions",
+      "Starred",
+      "Snoozed",
+      "Important",
+      "Sent",
+      "Outbox",
+      "Drafts",
+      "All mail",
+      "Spam",
+      "Bin",
+      "[Imap]/Sent",
+      "[Imap]/Trash",
+      "Settings",
+      "Help & feedback"
+    ];
+
     return Drawer(
       child: Column(
         children: <Widget>[
           UserAccountsDrawerHeader(
-              decoration: BoxDecoration(color: Colors.red),
-              accountName: name,
-              accountEmail: email,
-          currentAccountPicture: Icon(
-            Icons.account_circle,
-            size: 50.0,
-            color: Colors.white,
-          ),)
+            decoration: BoxDecoration(color: Colors.red),
+            accountName: name,
+            accountEmail: email,
+            currentAccountPicture: Icon(
+              Icons.account_circle,
+              size: 50.0,
+              color: Colors.white,
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: ListView.builder(
+                itemCount: drawerText.length,
+                padding: EdgeInsets.only(top:0.0),
+                itemBuilder: (context, position) {
+                  return ListTile(
+                    leading: drawerIcons[position],
+                    title: Text(drawerText[position],
+                        style: TextStyle(fontSize: 15.0)),
+                    onTap: () {
+                      this.setState(() {
+                        titleBarContent = drawerText[position];
+                      });
+                      Navigator.pop(context);
+                    },
+                  );
+                }),
+          )
         ],
       ),
     );
